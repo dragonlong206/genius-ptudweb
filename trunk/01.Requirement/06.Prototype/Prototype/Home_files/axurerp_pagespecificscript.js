a86432617eb6b9a9e3cc21eb48d4126bf983364a
lv.long.tn <lv.long.tn@92c3a7da-6dc5-7a73-75a6-1fe412d2b3f3>
2010-12-14 16:59:57 +0000

    Prototype Tìm nhanh tuyến

--- trunk/01.Requirement/06.Prototype/Prototype/Home_files/axurerp_pagespecificscript.js
+++ trunk/01.Requirement/06.Prototype/Prototype/Home_files/axurerp_pagespecificscript.js
@@ -42,32 +42,60 @@ function OnLoad(e) {
 
 }
 
-var u2 = document.getElementById('u2');
+eval(GetDynamicPanelScript('u0', 2));
 
-var u1 = document.getElementById('u1');
-gv_vAlignTable['u1'] = 'center';
-var u6 = document.getElementById('u6');
-gv_vAlignTable['u6'] = 'top';
-var u0 = document.getElementById('u0');
-
+var u10 = document.getElementById('u10');
+gv_vAlignTable['u10'] = 'top';
 var u5 = document.getElementById('u5');
-gv_vAlignTable['u5'] = 'top';
-var u4 = document.getElementById('u4');
 
-u4.style.cursor = 'pointer';
-if (bIE) u4.attachEvent("onclick", Clicku4);
-else u4.addEventListener("click", Clicku4, true);
-function Clicku4(e)
+u5.style.cursor = 'pointer';
+if (bIE) u5.attachEvent("onclick", Clicku5);
+else u5.addEventListener("click", Clicku5, true);
+function Clicku5(e)
 {
 
 if (true) {
 
-	self.location.href="SauDangNhap.html" + GetQuerystring();
+	SetPanelStateu0("pd1u0");
 
 }
 
 }
+
+var u0 = document.getElementById('u0');
 
 var u3 = document.getElementById('u3');
+
+var u9 = document.getElementById('u9');
+gv_vAlignTable['u9'] = 'center';
+var u6 = document.getElementById('u6');
+gv_vAlignTable['u6'] = 'top';
+var u1 = document.getElementById('u1');
+
+var u12 = document.getElementById('u12');
+gv_vAlignTable['u12'] = 'top';
+var u4 = document.getElementById('u4');
+
+var u11 = document.getElementById('u11');
+
+u11.style.cursor = 'pointer';
+if (bIE) u11.attachEvent("onclick", Clicku11);
+else u11.addEventListener("click", Clicku11, true);
+function Clicku11(e)
+{
+
+if (true) {
+
+	SetPanelStateu0("pd0u0");
+
+}
+
+}
+gv_vAlignTable['u11'] = 'top';
+var u7 = document.getElementById('u7');
+gv_vAlignTable['u7'] = 'top';
+var u2 = document.getElementById('u2');
+gv_vAlignTable['u2'] = 'center';
+var u8 = document.getElementById('u8');
 
 if (window.OnLoad) OnLoad();
