1fe6584cd4c41fc63b51847781f00fdf156a71c7
lv.long.tn <lv.long.tn@92c3a7da-6dc5-7a73-75a6-1fe412d2b3f3>
2010-12-27 18:24:27 +0000

    Chỉnh sửa store cho phù hợp với dữ liệu mới bảng PHAN_HOI_KHACH_HANG Thêm chức năng duyệt phản hồi

---
+++ trunk/03.Source code/BUS/PhanHoiBUS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DTO;
+using DAO;
+
+namespace BUS
+{
+    public class PhanHoiBUS
+    {
+        PhanHoiDAO daoPhanHoi = new PhanHoiDAO();
+
+        public void DuyetPhanHoi(int iMaPhanHoiKhachHang, int iMaTaiXe)
+        {
+            try
+            {
+                daoPhanHoi.DuyetPhanHoi(iMaPhanHoiKhachHang, iMaTaiXe);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ trunk/03.Source code/PTUDW_CTLH_C1/WUC/PhanHoi/wucDuyetPhanHoi.ascx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BUS;
+using DTO;
+
+namespace PTUDW_CTLH_C1.WUC.PhanHoi
+{
+    public partial class wucGhiNhanPhanHoi : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //this.rptPhanHoi.DataBind();
+        }
+
+        protected void ldsTaiXe_Selecting(object sender, LinqDataSourceSelectEventArgs e)
+        {
+            NhanVienBUS busNhanVien = new NhanVienBUS();
+            e.Result = busNhanVien.SelectTaiXesAll();
+        }
+
+        protected void rptPhanHoi_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            if (e.CommandName == "Duyet")
+            {
+                PHAN_HOI_KHACH_HANG phSelectedItem = (PHAN_HOI_KHACH_HANG)e.Item.DataItem;
+                PhanHoiBUS busPhanHoi = new PhanHoiBUS();
+                busPhanHoi.DuyetPhanHoi(phSelectedItem.MaPhanHoi, 2);
+            }
+        }
+    }
+}
