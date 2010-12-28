8a608f1de68a239e828f148d930855cf01feecbd
ngocnhi07.khtn <ngocnhi07.khtn@92c3a7da-6dc5-7a73-75a6-1fe412d2b3f3>
2010-12-28 17:46:48 +0000

    phân công xe

---
+++ trunk/03.Source code/PTUDW_CTLH_C1/WUC/Xe/wucPCXe.ascx
@@ -0,0 +1,38 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="wucPCXe.ascx.cs" Inherits="PTUDW_CTLH_C1.WUC.Xe.wucPCXe" %>
+<asp:LinqDataSource ID="LinqDataSource1" runat="server" 
+    ContextTypeName="DTO.CongTyLuHanhDataContext" TableName="XEs" 
+    Where="MaTaiXe @MaTaiXe" >
+    <WhereParameters>
+        <asp:ControlParameter ControlID="ddlPhanCong" DefaultValue="NULL" 
+            Name="MaTaiXe" PropertyName="SelectedValue" Type="String" />
+    </WhereParameters>
+
+</asp:LinqDataSource>
+
+<asp:DropDownList ID="ddlPhanCong" runat="server" AutoPostBack="True" >
+    <asp:ListItem Value="==NULL">
+        Chưa phân công
+    </asp:ListItem>
+    <asp:ListItem Value="==NULL">
+        Đã phân công
+    </asp:ListItem>
+    <asp:ListItem Value="==NULL">Tất cả</asp:ListItem>
+</asp:DropDownList>
+
+<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+    DataKeyNames="MaXe" DataSourceID="LinqDataSource1">
+    <Columns>
+        <asp:BoundField DataField="MaXe" HeaderText="MaXe" InsertVisible="False" 
+            ReadOnly="True" SortExpression="MaXe" />
+        <asp:BoundField DataField="BienSoXe" HeaderText="BienSoXe" 
+            SortExpression="BienSoXe" />
+        <asp:BoundField DataField="MaTaiXe" HeaderText="MaTaiXe" 
+            SortExpression="MaTaiXe" />
+        <asp:BoundField DataField="NgayNhap" HeaderText="NgayNhap" 
+            SortExpression="NgayNhap" />
+        <asp:BoundField DataField="MaLoaiXe" HeaderText="MaLoaiXe" 
+            SortExpression="MaLoaiXe" />
+        <asp:BoundField DataField="TinhTrang" HeaderText="TinhTrang" 
+            SortExpression="TinhTrang" />
+    </Columns>
+</asp:GridView>
---
+++ trunk/03.Source code/PTUDW_CTLH_C1/WUC/Xe/wucPhanCongXe.ascx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using BUS;
+using DTO;
+
+namespace PTUDW_CTLH_C1.WUC.Xe
+{
+    public partial class wucPhanCongXe : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.lblThongBao.Text = string.Empty;
+            gvXe.DataSource = ldsDSXe;
+            gvXe.DataBind();
+        }
+
+        protected void ldsTaiXe_Selecting(object sender, LinqDataSourceSelectEventArgs e)
+        {
+            
+        }
+
+
+
+        //protected void gvXe_RowCommand(object sender, GridViewCommandEventArgs e)
+        //{
+        //    if (e.CommandName == "Luu")
+        //    {
+        //        //Response.Write(e.CommandArgument);
+        //        this.lblThongBao.Text = "Cập nhật thành công tại vị trí: " + this.gvXe.SelectedIndex;
+        //        //this.gvXe.Selected
+        //    }
+        //}
+
+        protected void gvXe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+//            this.lblThongBao.Text = "Cập nhật thành công tại vị trí: " + this.gvXe.SelectedIndex;
+            DropDownList ddlTX = (DropDownList)this.gvXe.SelectedRow.Cells[4].FindControl("ddlTaiXe");            
+            int iMaTaiXe = int.Parse(ddlTX.SelectedValue.ToString());
+            int iMaXe = int.Parse(this.gvXe.SelectedDataKey.Value.ToString());
+            XeBUS busXe = new XeBUS();
+            bool bThanhCong = busXe.PhanXeChoTaiXe(iMaXe, iMaTaiXe);
+            if (bThanhCong)
+            {
+                this.lblThongBao.Text = "Đã phân xe " + this.gvXe.SelectedRow.Cells[0].Text
+                    + " cho tài xế " + ddlTX.SelectedItem.Text;
+                this.gvXe.DataBind();
+            }
+            else
+            {
+                this.lblThongBao.Text = "Phân công không thành công";
+            }
+        }
+
+        protected void ddlPhanCong_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int TinhTrangPhanCong = int.Parse(ddlPhanCong.SelectedValue);
+            if (TinhTrangPhanCong == 0)
+            {
+                gvXe.DataSource = ldsDSXe;
+                gvXe.DataBind();
+            }
+            else
+            {
+                if (TinhTrangPhanCong == 1)
+                {
+                    gvXe.DataSource = ldsXeDaPhanCong;
+                    gvXe.DataBind();
+                }
+                else
+                {
+                    if (TinhTrangPhanCong == 2)
+                    {
+                        gvXe.DataSource = ldsAllXe;
+                        gvXe.DataBind();
+                    }
+                }
+            }
+        }
+
+        protected void ldsXeDaPhanCong_Selecting(object sender, LinqDataSourceSelectEventArgs e)
+        {
+            XeBUS XeBUS = new XeBUS();
+            e.Result = XeBUS.LayDSXeDaPhanCong();
+        }
+
+        protected void ldsAllXe_Selecting(object sender, LinqDataSourceSelectEventArgs e)
+        {
+            
+        }
+
+       
+        protected void ldsDSXe_Selecting(object sender, LinqDataSourceSelectEventArgs e)
+        {
+            XeBUS XeBUS = new XeBUS();
+            e.Result = XeBUS.LayDSXeChuaPhanCong();
+        }
+
+        protected void ldsTaiXe_Selecting1(object sender, LinqDataSourceSelectEventArgs e)
+        {
+            NhanVienBUS NhanVienBUS = new NhanVienBUS();
+            e.Result = NhanVienBUS.LayDSTaiXeChuaPhanCong();
+        }
+    }
+}
